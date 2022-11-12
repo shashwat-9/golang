61f211ac128ffe3aa4cb7df8346b35661867790a
Shashwat Mishra <shashwatmishra91@gmail.com>
2022-11-12 22:36:13 +0530

    init commit

--- array.go
+++ array.go
@@ -1,1 +1,14 @@
 package main
+
+import "fmt"
+
+func main() {
+	var a = []int64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	fmt.Println(a)
+
+	var name = []string{"shashwat", "abc", "def", "ghi", "jhk"}
+
+	fmt.Println(name)
+
+}
